c294e4d65718f69856548d877e64de2b5546ad92
Aubree Herrion <aubreeherrion@gmail.com>
2022-07-30 12:30:21 -0500

    made changes to counter file.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,50 @@ $(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+  $('#submit-button').on('click', guessMade);
+  getGuess();
+  
 } // End handleReady
+let counter = 0;
+function getGuess(){
+  console.log('In getGuess');
+  counter ++
+  $('#guess-me').append(`
+     ${counter}
+  `)
+  $.ajax({
+    type: 'GET',
+    url: '/guess'
+  }).then(function(response){
+    $('#game-history').empty();
+    for(let guess of response){
+      $('#game-history').append(`
+        <ul>
+          <li>Player One: ${guess.playerOne}</li>
+          <li>Player Two: ${guess.playerTwo}</li>
+          <li>Player Three: ${guess.playerThree}</li>
+        </ul>
+      `)
+    }
+  })
+}
+
+function guessMade(){
+  
+  console.log('in guessMade');
+  $.ajax({
+    type: 'POST',
+    url: '/guess',
+    data:{
+      playerOne: $('#player-one').val(),
+      playerTwo: $('#player-two').val(),
+      playerThree: $('#player-three').val()
+    }
+  }).then(function(response){
+    getGuess();
+    $('#player-one').val('');
+    $('#player-two').val('');
+    $('#player-three').val('');
+    counter ++
+  });
+} // End guessMade
--- server/server.js
+++ server/server.js
@@ -9,7 +9,36 @@ app.use(bodyParser.urlencoded({extended:true}))
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
+let numberInputArray = [];
+
+function generateRandomNumber() {
+  function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
+  // return 8; // TODO: a random number
+}
+let answer = generateRandomNumber();
+
 // GET & POST Routes go here
+app.post('/reset', (req, res) => {
+  console.log(answer);
+  answer = generateRandomNumber();
+  // Clear history...
+  res.send(200);
+});
+
+app.get('/guess',(req, res) => {
+  res.send(numberInputArray);
+  
+});
+
+app.post('/guess', (req, res) => {
+  const guessInput = req.body;
+  console.log(req.body);
+  numberInputArray.push(guessInput);
+  res.sendStatus(200);
+});
+
 
 
 app.listen(PORT, () => {
